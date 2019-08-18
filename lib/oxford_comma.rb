3e523d86b0d830b28423051cd3c778688e335816
sami suliman <sami.bco@gmail.com>
2019-08-18 00:33:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,10 @@
 def oxford_comma(array)
- # array.join(",")
-
-
-if array.length < 2
-  array.join
-elsif array.lengh == 2 
-  array.insert(1, "and").join(" ")
-else 
-  str = ", and" + array.pop
-  str = array.join(", ") << str 
-end 
+ if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+  end
 end
